7c34831067165c9fcb49f78f5932b07b55210e58
maxwellc2003 <107020453+maxwellc2003@users.noreply.github.com>
2023-05-02 18:23:40 -0700

    sections and pages

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      {/* Front Page */}
+      <div></div>
+      {/* Skills Section */}
+      <div></div>
+      {/* Blog Featured Section */}
+      <div></div>
+      {/* Projects Section */}
+      <div></div>
+      {/* Contact */}
+      <div></div>
+    </main>
+  );
+};
+
+export default Home;
